3c38c099f9fd2875da0d934afa4376c52b0e2991
Agustin Paturlanne <aguspaturlanne@gmail.com>
2021-04-06 20:52:40 -0300

    x

--- ej4.py
+++
@@ -1,7 +0,0 @@
-cadena = input("Ingresa la clave (debe tener menos de 10 caracteres y no contener los símbolos:@ y !):")
-if len(cadena) > 10:
-    print("Ingresaste más de 10 carcateres")
-elif ("!" or "@") in cadena:
-    print("Ingresaste alguno de estos símbolos: @ o !" )
-else:
-    print("Ingreso OK")
