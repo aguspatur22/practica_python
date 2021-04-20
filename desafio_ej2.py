72050212aef8b893024683299f8cf2fcf4841395
Agustin Paturlanne <aguspaturlanne@gmail.com>
2021-04-20 11:20:42 -0300

    desafio ej2

---
+++ desafio_ej2.py
@@ -0,0 +1,19 @@
+import csv
+from collections import Counter
+
+archivo = open("appstore_games.csv", "r")
+csvreader = csv.reader(archivo, delimiter=',')
+
+encabezado = next(csvreader)
+
+juegos = {}
+
+for i in csvreader:
+    if i[6] not in ('' or ' '):
+        juegos[i[2]] = int(i[6])
+
+max = Counter(juegos).most_common(10)
+print('Los 10 juegos con mas User rating: ')
+print(dict(max))
+
+archivo.close()
